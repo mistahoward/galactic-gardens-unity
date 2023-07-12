daab975ab3245024a7e79e8bcfb6a78708aa3d60
mistahoward <me@alexhoward.dev>
2023-07-12 00:30:21 -0500

    creating entity and player classes

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public string Name;
+    public int Health;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : Entity
+{
+    protected override void Start()
+    {
+        base.Start();
+        // Any specific behavior for Player start
+    }
+
+
+    protected override void Update()
+    {
+        base.Update();
+        // Specific update behavior for Player
+    }
+}
