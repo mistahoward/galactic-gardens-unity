d43f4860a9598b9f64b3b7638d219321aedf88e9
mistahoward <me@alexhoward.dev>
2023-07-12 02:32:53 -0500

    adding movement, jumping, animations

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    public string Name;
-    public int Health;
-    public float Speed;
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,94 @@
 using UnityEngine;
 
-public class Player : Entity
+namespace Player
 {
-    protected override void Start()
+    public class Player : Entity
     {
-        base.Start();
-        // Any specific behavior for Player start
-    }
+        private Animator anim;
+        private CharacterController controller;
 
+        public float speed = 600.0f;
+        public float turnSpeed = 400.0f;
+        public float rotationSpeed = 10.0f;
+        private Vector3 moveDirection = Vector3.zero;
+        public float gravity = -20.0f;
+        public float jumpHeight = 2.0f;
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
 
-    protected override void Update()
-    {
-        base.Update();
-        // Specific update behavior for Player
+        private Vector3 velocity = Vector3.zero;
+        private bool isGrounded;
+
+        protected override void Start()
+        {
+            base.Start();
+            controller = GetComponent<CharacterController>();
+            anim = gameObject.GetComponentInChildren<Animator>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            // Get horizontal and vertical input (automatically works with WASD, arrow keys, and joystick)
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            // Create a direction vector from the input
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            // Calculate the target rotation based on the move direction
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+
+            // Smoothly rotate the player towards the target rotation at a certain speed
+            if (move != Vector3.zero) // Only rotate if moving to prevent snapping to default rotation
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            }
+
+            // Apply speed to the direction vector
+            move *= speed * Time.deltaTime;
+
+            // Move the player
+            controller.Move(move);
+
+            // Animation
+            if (move != Vector3.zero) // if there's any movement
+            {
+                anim.SetInteger("AnimationPar", 1);
+            }
+            else
+            {
+                anim.SetInteger("AnimationPar", 0);
+            }
+
+            // Jumping
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                Debug.Log("Jump");
+                // Apply upward force
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                // Trigger JumpStart animation
+                anim.SetTrigger("Jump");
+            }
+            Debug.Log("Is Grounded: " + isGrounded);
+
+            // Update IsGrounded parameter
+            anim.SetBool("IsGrounded", isGrounded);
+
+            // Apply gravity
+            velocity.y += gravity * Time.deltaTime;
+
+            // Move the player vertically
+            controller.Move(velocity * Time.deltaTime);
+        }
     }
 }
